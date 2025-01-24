5b24bf195bac8d6df980cf07d47929229889a08a
tbingle <sandmanthomas9@icloud.com>
2025-01-24 11:36:14 -0500

    Resubmission_Challange_1

---
+++ index.html/cc_1.js
@@ -0,0 +1,34 @@
+const employeelD = 3212
+let employeeName = "John Doe";
+var isActive = true;
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName); 
+console.log("EmployeelD:", employeelD, "Type:", typeof employeelD);
+console.log("Is active:", isActive, "Type:", typeof isActive); 
+
+let productName= "Flowers"
+const productprice= 8
+var isAvailable= true; 
+console.log("Product Name:", productName, "Type", typeof productName); 
+console.log("Product Price", productprice, "Type", typeof productprice);
+
+console.log("Is Available", isAvailable, "Type", typeof isAvailable);
+let accountBalance = 403982
+accountBalance -=2344
+console.log("After Withdraw $"+accountBalance);
+accountBalance +=4000
+console.log("After Deposit $"+ accountBalance); 
+accountBalance *=.12 
+console.log("After Dividens $"+accountBalance);
+accountBalance /=.25
+
+console.log("After Investments $"+accountBalance);
+let customerName = "Steven Pent"
+console.log("Customer Name",customerName, "Type", typeof customerName);
+let welcomeMessage = "Hello," +customerName + " Welcome to our flower shop"
+console.log(welcomeMessage);
+
+var Yes = true;
+let isLoggedin = Yes;
+console.log("Is Logged In", isLoggedin, "Type", typeof isLoggedin);
+let Websitestatus = "Operational"
+console.log("Website Status", Websitestatus, "Type", typeof Websitestatus);
